f02e74e3dda6de36f686b46e0537d41a53a0c591
JIAnnLee22 <947807614@qq.com>
2026-08-26 15:06:09 +0800

    fix tab jump

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -63,8 +63,8 @@ if cmp_ok then
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
           else
             fallback()
           end
@@ -72,6 +72,8 @@ if cmp_ok then
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
           else
             fallback()
           end
