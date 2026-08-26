1ee5db431def65155e6b108199803419b91b0cac
JIAnnLee22 <947807614@qq.com>
2026-08-26 18:16:38 +0800

    update nvim to simple config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,13 @@
-require('vim._core.ui2').enable({
-  enable = true, -- Whether to enable or disable the UI.
-})
-require("core.autocmd")
-require("core.basic")
-require("core.keymap")
-require("core.command")
-require("plugins")
+vim.g.mapleader = " "
+
+require("options")
+require("lsp")
+require("colorscheme")
+require("netrw")
+require("statusline")
+require("find")
+require("grep")
+require("autocommands")
+require("diagnostics")
+require("formatting")
+require("keymaps")
